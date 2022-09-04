52289b073422116a050f49aa630801ef9d671f31
sh3eys <sharaku3eyes@gmail.com>
2022-09-04 21:56:26 +0900

    init

---
+++ src/test/java/com/example/demo/CrudSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
